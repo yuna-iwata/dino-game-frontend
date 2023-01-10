de4336f6e0fffe6f08d2a706568212ad9ea90205
Yuna Iwata <yunaiwata@Yunas-MacBook-Pro.local>
2023-01-10 11:38:50 +0000

    ground moving

--- src/Pages/GamePage.js
+++ src/Pages/GamePage.js
@@ -1,16 +1,13 @@
 import Phaser from "phaser";
-import skyImg from "../assets/sky.png";
-import groundImg from "../assets/platform.png";
-import dudeImg from "../assets/dude.png";
+import backgroundImg from "../assets/background.png";
+import groundImg from "../assets/ground.png";
+import dinoImg from "../assets/dino-idle.png";
 import starImg from "../assets/star.png";
 import bombImg from "../assets/bomb.png";
 
 var player;
-var stars;
-var platforms;
-var cursors;
-var score = 0;
-var scoreText;
+const width = 800;
+const height = 600;
 
 class MyGame extends Phaser.Scene {
   constructor() {
@@ -18,109 +15,47 @@ class MyGame extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("sky", skyImg);
+    this.load.image("background", backgroundImg);
     this.load.image("ground", groundImg);
     this.load.image("star", starImg);
     this.load.image("bomb", bombImg);
-    this.load.spritesheet("dude", dudeImg, {
-      frameWidth: 32,
-      frameHeight: 48,
-    });
+    this.load.image("dino-idle", dinoImg);
   }
 
   create() {
-    this.add.image(400, 300, "sky");
+    this.speed = 10;
+    this.ground = this.add
+      .tileSprite(0, height, width, 26, "ground")
+      .setOrigin(0, 1);
+    // platforms.create(200, 200, "ground").setScale(2).refreshBody();
 
-    platforms = this.physics.add.staticGroup();
+    player = this.physics.add.sprite(0, height, "dino-idle").setOrigin(0, 1);
 
-    platforms.create(400, 568, "ground").setScale(2).refreshBody();
-
-    platforms.create(600, 400, "ground");
-    platforms.create(50, 250, "ground");
-    platforms.create(750, 220, "ground");
-
-    player = this.physics.add.sprite(100, 450, "dude");
-
-    player.setBounce(0.2);
     player.setCollideWorldBounds(true);
+    player.setGravityY(3000);
 
-    this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1,
+    this.jump();
+    // this.physics.add.collider(player, platforms);
+  }
+
+  jump() {
+    this.input.keyboard.on("keydown_SPACE", () => {
+      player.setVelocityY(-1000);
     });
-
-    this.anims.create({
-      key: "turn",
-      frames: [{ key: "dude", frame: 4 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    cursors = this.input.keyboard.createCursorKeys();
-
-    stars = this.physics.add.group({
-      key: "star",
-      repeat: 11,
-      setXY: { x: 12, y: 0, stepX: 70 },
-    });
-
-    stars.children.iterate(function (child) {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    });
-
-    scoreText = this.add.text(16, 16, "score: 0", {
-      fontSize: "32px",
-      fill: "#000",
-    });
-
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(stars, platforms);
-
-    this.physics.add.overlap(player, stars, this.collectStar, null, this);
   }
 
   update() {
-    if (cursors.left.isDown) {
-      player.setVelocityX(-160);
-
-      player.anims.play("left", true);
-    } else if (cursors.right.isDown) {
-      player.setVelocityX(160);
-
-      player.anims.play("right", true);
-    } else {
-      player.setVelocityX(0);
-
-      player.anims.play("turn");
-    }
-
-    if (cursors.up.isDown && player.body.touching.down) {
-      player.setVelocityY(-330);
-    }
-  }
-
-  collectStar(player, star) {
-    star.disableBody(true, true);
-
-    score += 10;
-    scoreText.setText("Score: " + score);
+    this.ground.tilePositionX += this.speed;
   }
 }
 
 const config = {
   type: Phaser.AUTO,
+  pixelArt: true,
+  transparent: true,
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 300 },
       debug: false,
     },
   },
